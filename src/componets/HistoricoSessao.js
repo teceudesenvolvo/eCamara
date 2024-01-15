6989d510572b54f15f250b80ea7f9711bd6b89f8
teceudesenvolvo <79342596+teceudesenvolvo@users.noreply.github.com>
2024-01-15 14:17:01 -0300

    sessao virtua

--- src/componets/HistoricoSessao.js
+++ src/componets/HistoricoSessao.js
@@ -14,28 +14,15 @@ class vacinaList extends Component {
         historico: [
             {
                 id: '1',
-                hora: '15:45',
-                descricao: 'O Conselheiro Valdomiro Távora votou acompanhando divergente no processo 09144/2015-0.',
-            },
-            {
-                id: '2',
-                hora: '15:45',
-                descricao: 'O Conselheiro Valdomiro Távora votou acompanhando divergente no processo 09144/2015-0.',
-            },
-            {
-                id: '3',
-                hora: '15:45',
-                descricao: 'O Conselheiro Valdomiro Távora votou acompanhando divergente no processo 09144/2015-0.',
-            },
-            {
-                id: '4',
-                hora: '15:45',
-                descricao: 'O Conselheiro Valdomiro Távora votou acompanhando divergente no processo 09144/2015-0.',
-            },
-            {
-                id: '5',
-                hora: '15:45',
-                descricao: 'O Conselheiro Valdomiro Távora votou acompanhando divergente no processo 09144/2015-0.',
+                sessao: '11ª Sessão Ordinária do 2º Semestre de 2023 da 3ª Sessão Legislativa da 19ª Legislatura',
+                votadas: `34`,
+                sobrestadas: '21',
+                retiradas: '1',
+                destacadas: '5',
+                impedidas: '2',
+                suspeitas: '0',
+                vista: '10',
+
             },
         ]
     }
@@ -61,8 +48,14 @@ class vacinaList extends Component {
                 }
             >
                 <div className='vacinaItem ' >
-                    <p className='valueProduct' >{historico.hora}</p>
-                    <p className='descricaoProduct' >{historico.descricao}</p>
+                    <p className='titleHistorico' >{historico.sessao}</p>
+                    <p className='descricaoHistorico' >Matérias Votadas: <b>{historico.votadas}</b></p>
+                    <p className='descricaoHistorico' >Matérias Sobrestadas: <b>{historico.sobrestadas}</b></p>
+                    <p className='descricaoHistorico' >Matérias Retiras de Pauta: <b>{historico.retiradas}</b></p>
+                    <p className='descricaoHistorico' >Matérias Destacadas: <b>{historico.destacadas}</b></p>
+                    <p className='descricaoHistorico' >Matérias com Impedimento: <b>{historico.impedidas}</b></p>
+                    <p className='descricaoHistorico' >Matérias com suspeição: <b>{historico.suspeitas}</b></p>
+                    <p className='descricaoHistorico' >Matérias em vista: <b>{historico.vista}</b></p>
                 </div>
             </li>
         )
@@ -71,7 +64,7 @@ class vacinaList extends Component {
         return (
             <>
                 <ul className='vistosHome historico-sessao-virtual'>
-                    <h1>Histórico da Sessão</h1>
+                    
                     {listCategories}
                 </ul>
             </>
